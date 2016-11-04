c4e15ef8d7aae2ed40ea9683f347642527ffdcff
James Leong <jyleong@sfu.ca>
2016-11-03 21:08:40 -0700

    begining authenticated users

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 
+#encyption gem for pw
+gem 'bcrypt', '3.1.11'
 # Use Puma as the app server
 gem 'bootstrap-sass','3.3.6'
 gem 'puma', '~> 3.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,19 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 
 class User < ApplicationRecord
-
+	before_save :defaults
+	before_save {self.email = email.downcase}
 	validates(:name, presence: true, length: {maximum: 30})
 	validates(:email, presence: true, length: {maximum: 40},
 		format: {with: VALID_EMAIL_REGEX}, uniqueness: 
 		{case_sensitive: false})
+	has_secure_password
+	validates :password, presence: true, length: { minimum: 6 }
+
+	private
+
+		def defaults
+			self.admin = false
+			self.currency = 0
+		end
 end
