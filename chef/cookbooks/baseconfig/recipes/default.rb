08b96ceec68d9aa90eb66e92f3b5b40c9ea01092
James Leong <jyleong@sfu.ca>
2016-11-07 21:00:44 -0800

    production deployment nginx for static assets

--- chef/cookbooks/baseconfig/recipes/default.rb
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -1,65 +1,65 @@
-# # # Make sure the Apt package lists are up to date, so we're downloading versions that exist.
-# cookbook_file "apt-sources.list" do
-#   path "/etc/apt/sources.list"
-# end
-# execute 'apt_update' do
-#   command 'apt-get update'
-# end
+# # Make sure the Apt package lists are up to date, so we're downloading versions that exist.
+cookbook_file "apt-sources.list" do
+  path "/etc/apt/sources.list"
+end
+execute 'apt_update' do
+  command 'apt-get update'
+end
 
 # # Base configuration recipe in Chef.
-# package "wget"
-# package "ntp"
-# cookbook_file "ntp.conf" do
-#   path "/etc/ntp.conf"
-# end
-# execute 'ntp_restart' do
-#   command 'service ntp restart'
-# end
+package "wget"
+package "ntp"
+cookbook_file "ntp.conf" do
+  path "/etc/ntp.conf"
+end
+execute 'ntp_restart' do
+  command 'service ntp restart'
+end
 
-# package "nginx"
-# package "git"
-# package "libpq-dev"
-# package "postgresql"
+package "nginx"
+package "git"
+package "libpq-dev"
+package "postgresql"
 
-# cookbook_file "nginx-default" do
-# 	path "/etc/nginx/sites-available/default"
-# end
-# execute 'nginx_restart' do
-# 	command 'service nginx restart'
-# end
+cookbook_file "nginx-default" do
+	path "/etc/nginx/sites-available/default"
+end
+execute 'nginx_restart' do
+	command 'service nginx restart'
+end
 
-# execute 'setup_db' do
-# 	command 'echo "CREATE DATABASE mydb; CREATE USER ubuntu; GRANT ALL PRIVILEGES ON DATABASE mydb TO ubuntu;" | sudo -u postgres psql'
-# end
+execute 'setup_db' do
+	command 'echo "CREATE DATABASE mydb; CREATE USER ubuntu; GRANT ALL PRIVILEGES ON DATABASE mydb TO ubuntu;" | sudo -u postgres psql'
+end
 
-# execute 'nginx_reload' do
-# 	command 'service nginx reload'
-# end
+execute 'nginx_reload' do
+	command 'service nginx reload'
+end
 
-# # # Rails setup
+# # Rails setup
 
-# package "ruby-dev"
-# package "sqlite3"
-# package "libsqlite3-dev"
-# package "zlib1g-dev"
-# package "nodejs"
-# package "build-essential"
+package "ruby-dev"
+package "sqlite3"
+package "libsqlite3-dev"
+package "zlib1g-dev"
+package "nodejs"
+package "build-essential"
 
-# execute 'bundler install' do
-# 	command 'gem install bundler --conservative'
-# end
+execute 'bundler install' do
+	command 'gem install bundler --conservative'
+end
 
-# execute 'bundler' do
-# 	user 'ubuntu'
-# 	cwd '/home/ubuntu/project'
-# 	command 'bundle install'
-# end
+execute 'bundler' do
+	user 'ubuntu'
+	cwd '/home/ubuntu/project'
+	command 'bundle install'
+end
 
-# execute 'migrate' do
-# 	user 'ubuntu'
-# 	cwd '/home/ubuntu/project'
-# 	command 'rails db:migrate RAILS_ENV=production'
-# end
+execute 'migrate' do
+	user 'ubuntu'
+	cwd '/home/ubuntu/project'
+	command 'rails db:migrate RAILS_ENV=production'
+end
 
 cookbook_file "unicorn_rails" do
   path "/etc/init.d/unicorn_rails"
