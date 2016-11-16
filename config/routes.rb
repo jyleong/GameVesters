db3a9b61559b6ef10c09eed5f7744b04dc1172d7
Tyler Wong <TylerWong@d207-023-162-121.wireless.sfu.ca>
2016-11-16 15:09:03 -0800

    Questions: Create, view questions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   resources :user_stocks, except: [:show, :edit,:update]
   resources :stocks
+  resources :questions 
+  get '/questions/:id/display_question', to: 'questions#display_question', as: 'display_question'
+	
+
   get 'sessions/new'
 
 
@@ -12,11 +16,7 @@ Rails.application.routes.draw do
 	get '/about', to: 'static_pages#about'
 	get '/contact', to: 'static_pages#contact'
 	get '/signup', to: 'users#new'
-	get '/question', to: 'questions#display_question'
-
-
 	post '/signup', to: 'users#create'
-
 	get 'login', to: 'sessions#new'
 	post '/login', to: 'sessions#create'
 	delete '/logout', to: 'sessions#destroy'
