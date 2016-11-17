ffe1e2b169a7f66446ede33e91da29418b31db55
Carlson Chan <carlsonc@sfu.ca>
2016-11-16 16:52:24 -0800

    Added homepage trending tickers and user stocks and added some css

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,12 @@ class StaticPagesController < ApplicationController
   	#  	@micropost  = current_user.microposts.build
    #   	@feed_items = current_user.feed.paginate(page: params[:page], per_page: 15)
   	# end
+    @stocks = StockQuote::Stock.quote(["optt", "dcix", "esea", "ship","drys"])
+
+    if logged_in?
+      @user_stocks = current_user.stocks
+      @user = current_user
+    end
   end
 
   def help
