0dbed43f0948dfb934249a800359f6d3d12f3856
Riku Kenju <fresh.ham.man@gmail.com>
2016-11-22 19:10:07 -0800

    remove redis

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,38 +1,22 @@
 class LeaderboardsController < ApplicationController
-  before_action :query_options
   before_action :logged_in_user, only: [:show]
 
   def show
-    @lb = Boards.default_leaderboard
-    @entries = entry_service.execute(query_options)
+    @leaders = sort_by_percent_increase
 
     respond_to do |format|
       format.html do
         paginate
       end
       format.json do
-        render json: @entries
+        render json: @leaders
       end
     end
   end
 
   private
 
-  def query_options
-    @limit = [params.fetch(:limit, 10).to_i, 100].min
-    @page = params.fetch(:page, 1).to_i
-    { page: @page, limit: @limit }
-  end
-
-  def paginate
-    pager = Kaminari.paginate_array(
-      @entries,
-      total_count: @lb.total_members)
-
-    @page_array = pager.page(@page).per(@limit)
-  end
-
-  def entry_service
-    Boards::GetAllService.new
+  def sort_by_percent_increase
+    sorted_users = User.order('percent_increase desc').pluck(:name, :percent_increase)
   end
 end
