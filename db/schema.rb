c98e002b55b81379965127ecef7c4ce661093240
Carlson Chan <carlsonc@sfu.ca>
2016-11-27 13:26:38 -0800

    Updated amount change color

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161121211613) do
 
   create_table "feed_entries", force: :cascade do |t|
@@ -22,7 +21,6 @@ ActiveRecord::Schema.define(version: 20161121211613) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
-
 
   create_table "notifications", force: :cascade do |t|
     t.integer  "user_id"
